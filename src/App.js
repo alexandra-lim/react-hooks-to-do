de97bb6e126a815f821fcc8b26aed38a10c36532
Alexandra Lim <alexandra.lim@gmail.com>
2020-08-30 18:11:51 -0400

    added functionality to add and update new list item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
-const Todo = ({ todo, index }) => {
-  return <div className='todo'>{todo.text}</div>;
+const Todo = ({ todo, index, completeTodo }) => {
+  return (
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className='todo'
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
+};
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='test'
+        className='input'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder='Add To Do'
+      />
+    </form>
+  );
 };
 
 const App = () => {
@@ -21,12 +54,29 @@ const App = () => {
     },
   ]);
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
   return (
     <div className='app'>
       <div class='todo-list'>
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
